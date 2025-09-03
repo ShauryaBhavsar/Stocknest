daff43e9c5b81cd42bbbb29c3008510b3848cd21
ShauryaBhavsar <118375379+ShauryaBhavsar@users.noreply.github.com>
2025-09-03 22:59:00 +0530

    Delete dashboard directory

--- dashboard/src/components/Logout.js
+++
@@ -1,7 +0,0 @@
-import { useCookies } from "react-cookie";
-function Logout (){
-    const [cookies, removeCookie] = useCookies([]);
-    removeCookie("token");
-    window.location.href="http://localhost:3000/login"
-  };
-  export default Logout;
